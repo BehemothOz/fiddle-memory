3765a203338e010b065add7d888073a49ca9315e
behemothoz <behemothoz@yandex.ru>
2020-10-31 20:12:14 +0300

    remove constants files

--- components/Cell/index.js
+++ components/Cell/index.js
@@ -1,5 +1,30 @@
-import { Status, isClosed } from './status';
 import { getClassName } from './classes';
+
+export const Status = {
+    OPEN: 'open',
+    FAILED: 'failed',
+    DONE: 'done',
+    CLOSED: 'closed',
+};
+
+export const isOpen = cell => cell.status === Status.OPEN;
+export const isFailed = cell => cell.status === Status.FAILED;
+export const isDone = cell => cell.status === Status.DONE;
+export const isClosed = cell => cell.status === Status.CLOSED;
+
+const ClassByStatus = {
+    [Status.OPEN]: 'open',
+    [Status.FAILED]: 'fail',
+    [Status.DONE]: 'done',
+    [Status.CLOSED]: 'close',
+};
+
+const UNKNOWN_CLASS = 'unknown';
+
+export const getClassName = status => {
+    if (status && status in ClassByStatus) return ClassByStatus[status];
+    return UNKNOWN_CLASS;
+};
 
 /*
     Name Cell will be reserved for type
