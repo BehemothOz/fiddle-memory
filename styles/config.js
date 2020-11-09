d55a018f544b06bba0b632b2734bec5de35c3b35
behemothoz <behemothoz@yandex.ru>
2020-11-09 21:14:40 +0300

    start config

---
+++ styles/config.js
@@ -0,0 +1,22 @@
+const config = {
+    palette: {
+        common: {
+            white: '',
+            black: '',
+        },
+        grey: {
+            [0]: '',
+            [100]: '',
+        },
+        red: {
+            [0]: '',
+            [100]: '',
+        },
+    },
+    text: {
+        primary: '',
+        secondary: '',
+        disabled: '',
+        hint: '',
+    },
+};
